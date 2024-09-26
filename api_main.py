d72c6131b3f343539d32a15857249169eeb54eaa
nsemenov <nikita.semenov@etu.u-bordeaux.fr>
2024-09-26 17:21:41 +0200

    added multiform n_color field

--- api_main.py
+++ api_main.py
@@ -29,12 +29,12 @@ def image_color_pallete():
     print(file.filename)
     file.save(file.filename)
     print(request)
-    data = request.data
-    print(data)
+    number_of_colors = request.form['n_colors']
+    #print(data)
     #json_file = request.load(json_data)
     #json_data = json.load(json_file)
 
-    pallete = image_utils.pallette(file.filename, 2)
+    pallete = image_utils.pallette(file.filename, number_of_colors)
     print(pallete)
     for i in range(len(pallete)):
         LAB = color_matching.rgb2lab(pallete[i])
